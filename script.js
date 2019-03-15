f53def92a121541e8c5fb79f4637ef931a8d9e61
magkuna <magkuna@gmail.com>
2019-03-15 10:28:05 +0100

    exercise 1

---
+++ script.js
@@ -0,0 +1,6 @@
+const collection = document.querySelector('h1').style.color = 'red';
+const collection2 = document.querySelectorAll ('p');
+let i;
+for (i = 0; i < collection2.length; i++){
+    collection2[i].style.backgroundColor= 'yellow'
+}
