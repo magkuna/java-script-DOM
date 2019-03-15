ed5476ac512a7823a4bcc82ebe68f0ad6ffd158c
magkuna <magkuna@gmail.com>
2019-03-15 10:37:26 +0100

    exercise1  decorations

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const collection = document.querySelector('h1').style.color = 'red';
+const collection = document.querySelector('h1');
+collection.style.color = 'red';
+collection.style.textDecoration = 'underline';
+
 const collection2 = document.querySelectorAll ('p');
 let i;
 for (i = 0; i < collection2.length; i++){
-    collection2[i].style.backgroundColor= 'yellow'
+    collection2[i].style.backgroundColor= 'yellow';
 }
+
+collection2[0].style.textDecoration= 'underline';
