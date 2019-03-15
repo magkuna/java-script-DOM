e2f667adf8a1c88579cd1cf5c4aaf73ff215670a
magkuna <magkuna@gmail.com>
2019-03-15 11:49:25 +0100

    exercise 2

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const collection = document.querySelector('h1');
-collection.style.color = 'red';
-collection.style.textDecoration = 'underline';
-
-const collection2 = document.querySelectorAll ('p');
-let i;
-for (i = 0; i < collection2.length; i++){
-    collection2[i].style.backgroundColor= 'yellow';
-}
-
-collection2[0].style.textDecoration= 'underline';
+window
+.addEventListener(
+    'dblclick', () => alert('hello, world'));
